f68ae1b466570b4ee230b3f9d055d4859da94ad8
matias arenhard <matias_arenhard@hotmail.com>
2018-10-12 15:47:59 -0300

    first version off scrap logic

--- app/controllers/api/v1/quotes_controller.rb
+++ app/controllers/api/v1/quotes_controller.rb
@@ -6,18 +6,29 @@ require 'mechanize'
 
 class Api::V1::QuotesController < ApplicationController
   def index
-    mechanize = Mechanize.new
-    mechanize.get('http://quotes.toscrape.com') do |page|
-      page.search(".quote").map do |quote|
-        quote = quote.children[1].text
-        author = quote.children[3].text
-        #author_about = quote.children[3].text
-        tags = quote.children[5].text
-        byebug
+    response = []
+    params[:tag].split(",").map do |tag_search|
+      verify = Quote.where(tags: /#{tag_search}/)
+      if verify.count > 0
+        response.push(verify)
+      else
+        mechanize = Mechanize.new
+        mechanize.get("http://quotes.toscrape.com/tag/#{tag_search}/page/1/") do |page|
+          page.search(".quote").map do |quote|
+            description_quote = quote.search(".text").text
+            author = quote.search(".author").text
+            start = quote.children[3].to_s.index("href=").to_i + 5
+            stop = quote.children[3].to_s.index('">(').to_i
+            author_about = quote.children[3].to_s[start...stop]
+            tags_aux = []
+            quote.search(".tag").map do |tag|
+              tags_aux.push(tag.text)
+            end
+            create_quote = Quote.create(author: author, author_about: author_about, quote: description_quote, tags: tags_aux)
+            response.push(create_quote)
+          end
+        end
       end
     end
-  end
-
-  def create
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,6 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'quotes/index'
-      get 'quotes/create'
     end
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
